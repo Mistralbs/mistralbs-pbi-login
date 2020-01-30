dee40db1d62f0e058e6cffe8481c613b18935807
Rubén Soler <ruben.soler@exp360.com>
2020-01-30 10:47:01 +0100

    Fix error message bad request

--- index.js
+++ index.js
@@ -68,7 +68,7 @@ router.post('/login', bodyParser.urlencoded({ extended: false }), function (req,
     if (req.session.user) {
       return res.redirect('/dashboard');
     } else {
-      throw new Error('Forbidden');
+      throw new Error('Bad Request');
     }
 
   } catch (err) {
